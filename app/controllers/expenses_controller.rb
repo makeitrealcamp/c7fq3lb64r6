93f60808868930b8ac9b3ae8cfd669e3aec9f5bf
dacardenasa <diego.cardenasaleg@gmail.com>
2020-03-31 16:13:29 -0500

    Filt expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,36 @@
 class ExpensesController < ApplicationController
+
   def index
-   @expenses = Expense.order("date DESC")
+    if params[:concept].blank? and params[:category_id].blank?
+      @expenses = Expense.order("date DESC")
+      render :index
+    elsif params[:category_id].blank?
+      @expenses = Expense.where(concept: castparam(params[:concept]))
+      render :index
+    elsif params[:concept].blank?
+      @expenses = Expense.where(category_id: params[:category_id])
+      render :index
+    else
+      @expenses = Expense.where(category_id: params[:category_id], concept: castparam(params[:concept]))
+      render :index
+    end
+    @expenses = Expense.order("date DESC")
   end
+
+  private 
+  def castparam(string)
+    string = string.downcase
+    
+    case string
+    when "burral"
+      string = "Hamburguesas el burral"
+    when "gasolina"
+      string = string.capitalize
+    when "lamparas"
+      string = string.capitalize
+    else
+      string
+    end
+  end
+
 end
